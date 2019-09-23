737abc2e828adae6122ffee37d1fc569b7c6adf7
islekburak <48826672+islekburak@users.noreply.github.com>
2019-09-23 22:45:33 +0300

    Update convert_individual_fastas_by_taxids.py

--- convert_individual_fastas_by_taxids.py
+++ convert_individual_fastas_by_taxids.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 from collections import defaultdict
 
-#open file1, using col2 as ids and col1 appending to defaultdict values
+#open ids.txt, using col2 as taxids and col1 appending to defaultdict values
 with open("ids.txt", "r") as f:
     ids = defaultdict(list)   
     for line in f:
         ids[line.strip().split("\t")[0]].append(line.strip().split("\t")[1])
 
-# open file2 fasta, put sequences into dictionary with col1 ids as keys
+# open linearized fasta file, put sequences into dictionary with col1 ids as keys
+# to linearize fasta file before run this command;
+# awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < file.fasta > linearized_file.fasta
 with open("outscan_complete_seq_linearized.fasta", "r") as fasta:
     seqs = {}
     for line in fasta:
         if line.startswith(">"):
             seqs[line.strip().split("|")[1]] = next(fasta).strip()
 
-# for each id (used as new filename), print out matching col1 ids and sequences to new file
+# for each taxid (used as new filename), print out matching col1 ids and sequences to new file
 for i in ids:
     with open(i + ".fasta", "w") as out:
         for n in range(len(ids[i])):
