51edb957de7cb5ffb595241c380a9d4205e5328e
islekburak <48826672+islekburak@users.noreply.github.com>
2019-09-30 10:01:15 +0300

    Update make_blastdb.py

--- make_blastdb.py
+++ make_blastdb.py
@@ -5,5 +5,5 @@ fasta_files_for_blast_db= os.listdir(fasta_files_for_blastp_db_path)
 for file in fasta_files_for_blast_db:
     if "with_tax_info" not in file:
         continue
-    bash = "makeblastdb -in " + fasta_files_for_blastp_db_path + file +" -parse_seqids -dbtype prot -out /cta/users/aylinbircan/blastdb_single_isoform/blastdb_for_class_F/ " + file.split("_")[0]
+    bash = "makeblastdb -in " + fasta_files_for_blastp_db_path + file +" -parse_seqids -dbtype prot -out /cta/users/islekburak/blastdb_for_class_F/ " + file.split("_")[0]
     process = subprocess.calls(bash, shell=True)
