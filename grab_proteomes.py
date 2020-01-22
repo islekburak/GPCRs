5f4de73a7e8fd8b30e8b6e4bd3740e3a10d7830a
islekburak <48826672+islekburak@users.noreply.github.com>
2020-01-22 20:18:39 +0300

    revised

--- grab_proteomes.py
+++ grab_proteomes.py
@@ -7,8 +7,9 @@ ftp.cwd("pub/databases/uniprot/current_release/knowledgebase/reference_proteomes
 filenames=ftp.nlst("*[0-9].*fasta.gz")
 
 for filename in filenames:
-    local_filename = os.path.join('islekburak@10.39.60.250 : cta/users/islekburak/codes/new/', filename)
-    file = open(local_filename, 'wb')
-    ftp.retrbinary('RETR '+ filename, file.write)
+    local= os.path.join("islekburak@10.39.60.250:/cta/users/islekburak/codes/new/",filename)
+    local_filename= "'" + local + "'"
+    file=open(local_filename,"wb")
+    ftp.retrbinary("RETR" + " " +filename, file.write)
     file.close()
 ftp.quit()
